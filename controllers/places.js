bb4bec8b25f961da87bc773f42159b199439d380
Priyanka Shah <priyanka2003shah@gmail.com>
2022-05-24 03:36:54 -0400

    part 3 submit

--- controllers/places.js
+++ controllers/places.js
@@ -2,8 +2,26 @@ const router = require('express').Router()
 //first route in places.js. We can use the index page 
 //(i.e., the GET route 
 //that will eventually show a list of all places).
+// GET /places
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
+
+  })
+  
+  
 
 module.exports = router
