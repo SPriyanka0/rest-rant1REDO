a65c446007f86396703de8a99fd710cb94309f85
Priyanka Shah <priyanka2003shah@gmail.com>
2022-09-18 00:19:00 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -14,6 +16,6 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.render('error404')
 })
-
+//listen for connections
 app.listen(process.env.PORT)
 
--- views/default.jsx
+++ views/default.jsx
@@ -1,17 +1,19 @@
 const React = require('react')
-//Create a stub function called Def that has one parameter called html as you see in the code snippet below.
+
 function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                {/* cdn link forr latest bootstarap added */}
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
-//Export the Def function you just created.
+}
+
 module.exports = Def
+
