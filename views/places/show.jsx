ea4f37d10e93255f4bd4791a7c31e02d740e5497
Priyanka Shah <priyanka2003shah@gmail.com>
2022-10-12 13:13:04 -0400

    part10 submit

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,50 +1,61 @@
 const React = require('react')
 const Def = require('../default')
 //displays information of specific place wwith connected routes
+let comments = (
+  <h3 className="inactive">
+    No comments yet!
+  </h3>
+)
+
 function show(data) {
-   let comments = (
-    <h3 className="inactive">No comments yt!</h3>
-   )
-   if (data.place.comments.length) {
-    comments = data.place.comments.map(c => {
-      return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <stong>- {c.author}</stong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
-        </div>
-      )
-    })
-  }
+  //  let comments = (
+  //   <h3 className="inactive">No comments yt!</h3>
+  //  )
+  //  if (data.place.comments.length) {
+  //   comments = data.place.comments.map(c => {
+  //     return (
+  //       <div className="border">
+  //         <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+  //         <h4>{c.content}</h4>
+  //         <h3>
+  //           <stong>- {c.author}</stong>
+  //         </h3>
+  //         <h4>Rating: {c.stars}</h4>
+  //       </div>
+  //     )
+  //   })
+  // }
     return (
-        <Def>
-            <main>
-                <h1>
-                    {data.place.name}
-                </h1>
-                <img src={data.place.pic} alt={data.place.name}/>
-                <h3>Located in {data.place.city}, {data.place.state}</h3>
-                <h3>Description</h3>
-                <h3>{data.place.showEstablished()}</h3>
-                <h4>Serving {data.place.cuisines}</h4>
-                <h3>Rating</h3>
-                <p> not rated yet </p>
-                <h3>Comments</h3>
-                {comments}
-                {/* href allows us to acess data.id */}
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
-            </main>
-        </Def>
+      <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Comments</h2>
+              {comments}
+            <br />
+            <h2>Description</h2>
+            <h4>Serving: {data.place.cuisines}</h4>
+          <br />
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            Edit
+          </a>{" "}
+          {` `}
+          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <button type="submit" className="btn btn-danger">
+              Delete
+            </button>
+          </form>
+          </div>
+        </div>
+      </main>
+    </Def>
     )
 }
 
