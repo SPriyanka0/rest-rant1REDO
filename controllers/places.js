c4b9511616ee86aabd6222bb6017ea52c1af9f0d
Priyanka Shah <priyanka2003shah@gmail.com>
2022-10-12 13:31:53 -0400

    added stars

--- controllers/places.js
+++ controllers/places.js
@@ -35,11 +35,24 @@ router.post('/', (req, res) => {
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+// router.get('/:id', (req, res) => {
+//   let id = Number(req.params.id)
+//   if (isNaN(id)) {
+//     res.render('error404')
+//   }
+//   else if (!places[id]) {
+//     res.render('error404')
+//   }
+//   else {
+//     //passes data to vieww
+//     res.render('places/show', { place: places[id], id })
+//   }
+// })
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
   .populate('comments')
   .then(place => {
-      console.log(place.comments)
+      //console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -78,12 +91,26 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
